2cc63b3888a9b87f18de565b3d1eac2da9d75777
Robert Chiniquy <rchiniquy@yahoo.com>
2011-07-15 08:20:18 -0700

    rch - tests still broken - looks like the bymap and byplaylist api calls shouldn't be seen as returning player data but player statistics data

--- spec/MetadataSpec.js
+++ spec/MetadataSpec.js
@@ -1,3 +1,4 @@
+
 var
   $ = require('../node_modules/jquery/dist/node-jquery.js'),
   MockApiClient = require('../spec/MockApiClient.js').MockApiClient,
--- spec/PlayerDaoSpec.js
+++ spec/PlayerDaoSpec.js
@@ -33,30 +33,103 @@ describe('PlayerDao', function() {
     asyncSpecWait();
   });
   
-  it('should detect existence', function() {
+  /*
+  it('should load with get', function() {
     var
       existed,
       pd = PlayerDao(MockApiClient),
-      player; 
-            
-    pd.get(function(err, obj) {
-      player = obj;
+      player;
       
-      $.when(pd.exists())
-        .fail(function(){
-          existed = false;
-          asyncSpecDone();
-          expect(existed).toEqual(true); 
-        })
-        .done(function(){
-          existed = true;
-          asyncSpecDone();
-          expect(existed).toEqual(true); 
-        });      
-    });      
+    $.when(        
+      pd.get('cioj', function(err, obj) {
+        player = obj;
+        
+
+        $.when(pd.exists())
+          .fail(function(){
+            existed = false;
+            asyncSpecDone();
+            expect(existed).toEqual(true); 
+          })
+          .done(function(){
+            existed = true;
+            asyncSpecDone();
+            expect(existed).toEqual(true); 
+          });      
+      })
+    ).done(function() {
+      expect(true).toEqual(true);         
+      asyncSpecDone();
+    })
+    .fail(function() {
+      expect(1).toEqual(2);
+      asyncSpecDone();
+    });
       
     asyncSpecWait();    
   });
+  */
+
+  it('should be able to load from getDetailsNoStatsOnly', function() {
+    var
+      pd = PlayerDao(MockApiClient),
+      player;
+
+    $.when(
+      pd.getDetailsNoStatsOnly(
+        'cioj', 
+        function(err, obj) {
+          player = obj
+        }      
+      )
+    ).done(function() {
+      expect(player.service_tag).toEqual('BAIT');
+      asyncSpecDone();        
+    });
+
+    asyncSpecWait();        
+  });
+
+  it('should be able to load from getDetailsByPlaylistOnly', function() {
+    var
+      pd = PlayerDao(MockApiClient),
+      player;
+
+    $.when(
+      pd.getDetailsByPlaylistOnly(
+        'cioj', 
+        function(err, obj) {
+          player = obj
+        }      
+      )
+    ).done(function() {
+      expect(player.service_tag).toEqual('BAIT');
+      asyncSpecDone();        
+    });
+
+    asyncSpecWait();        
+  });
+/*
+  it('should be able to load from getDetailsByMapOnly', function() {
+    var
+      pd = PlayerDao(MockApiClient),
+      player;
+
+    $.when(
+      pd.getDetailsByMapOnly(
+        'cioj', 
+        function(err, obj) {
+          player = obj
+        }      
+      )
+    ).done(function() {
+      expect(player.service_tag).toEqual('BAIT');
+      asyncSpecDone();        
+    });
+
+    asyncSpecWait();        
+  });
+*/
 
 
 });
